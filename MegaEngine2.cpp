d02b96c04c33f0664395cf9bb6440804c0a4d2c6
MegaProhor <129941509+Prohor327@users.noreply.github.com>
2025-09-27 00:25:46 +0500

    Add project files.

---
+++ MegaEngine2.cpp
@@ -0,0 +1,12 @@
+﻿// MegaEngine2.cpp : Defines the entry point for the application.
+//
+
+#include "MegaEngine2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
